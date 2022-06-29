443f24ee3fc50408787d44a5ab8750d11236b76a
Legolass322 <l.e.g.o.l.a.s.s.dm@gmail.com>
2022-06-29 17:42:51 +0300

    db example: sql+hs

--- src/DB/TableCreator.hs
+++ src/DB/TableCreator.hs
@@ -1,5 +1,50 @@
 module DB where
 
+-- dep
+import Control.Applicative
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
+
+-- test data (compare with table)
+data Test = Test 
+    { id
+    , str
+    }
+instance Show Test where
+    show test = mconcat 
+        [ show $ id test
+        , ", "
+        , str test
+        ]
+instance FromRow Test where
+    fromRow = Test 
+        <$> field 
+        <*> field
+-- test of functionality
+
+-- Appling action to db file
+withConn :: String -> (Connection -> IO ()) -> IO ()
+withConn dbName action = do
+    conn <- open dbName
+    action conn
+    close conn 
+
+-- Pushing string to db
+addTest :: String -> IO ()
+addTest str = withConn "db.db" $ 
+    \conn -> do
+        execute conn "INSERT INTO test (str) VALUES (?);" (Only str)
+        print "string added"
+
+-- Printing all strings from db
+printTest :: IO ()
+printTest = withConn "db.db" $
+    \conn -> do
+        rows <- query_ conn "SELECT * FROM test;" :: IO [Test]
+        mapM_ print rows
+
+-- exec in command line
+-- sqlite3 db.db < CreateTable.sql
 createTable = undefined
 
 test :: IO()
